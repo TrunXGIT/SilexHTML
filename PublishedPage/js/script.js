51e2616d5d696619b81a7866225f22115fbe4fd5
TrunXGIT <TrunX@live.de>
2021-01-14 22:09:48 +0100

    Batch update

--- PublishedPage/js/script.js
+++ PublishedPage/js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-legal-disclosure","displayName":"Legal-Disclosure","link":{"linkType":"LinkTypePage","href":"#!page-legal-disclosure"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-legal-disclosure","displayName":"Legal-Disclosure","link":{"linkType":"LinkTypePage","href":"#!page-legal-disclosure"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
